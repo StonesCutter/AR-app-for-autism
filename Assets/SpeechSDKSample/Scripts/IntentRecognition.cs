ad7d97748f68757c6159e16f68a390f3554092b4
matteomalandra <62064849+matteomalandra@users.noreply.github.com>
2022-01-29 10:48:30 +0100

    ca

--- Assets/SpeechSDKSample/Scripts/IntentRecognition.cs
+++ Assets/SpeechSDKSample/Scripts/IntentRecognition.cs
@@ -189,10 +189,13 @@ public class IntentRecognition : MonoBehaviour {
         // Use the inspector fields to manually set these values with your subscription info.
         // If you prefer to manually set your LUIS AppId, Key and Region in code,
         // then uncomment the three lines below and set the values to your own.
-        LUISAppId = "7b067b7f-39b0-4d42-ba5b-ca770a73ad6a";
-        LUISAppKey = "b0c7e37ef0a94996a8a00a4523a81395";
+        LUISAppId = "57a4fdc6-b228-437f-a59e-9c493fc48ee1";
+        LUISAppKey = "092b8d29de294b6b9b29c26f50723356";
         LUISRegion = "westeurope";
-
+/*                LUISAppId = "7b067b7f-39b0-4d42-ba5b-ca770a73ad6a";
+                LUISAppKey = "b0c7e37ef0a94996a8a00a4523a81395";
+                LUISRegion = "westeurope";
+*/
     }
 
     // Use this for initialization
